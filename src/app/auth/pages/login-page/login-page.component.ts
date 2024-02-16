0fe29d952be5ecbb8a57beb58fd6167a7c860bfe
gaburra-m <gaburra.m@gmail.com>
2024-02-15 18:07:32 -0600

    inerfaces y authService

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -7,13 +10,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPageComponent {
   private _fb = inject(FormBuilder);
+  private _authService = inject(AuthService);
 
   public myForm: FormGroup = this._fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['fernando@gmail.com', [Validators.required, Validators.email]],
+    password: ['1234567', [Validators.required, Validators.minLength(6)]],
   });
 
   login() {
-    console.log(this.myForm.value);
+    const { email, password } = this.myForm.value;
+    this._authService.login(email, password).subscribe({
+      next: () => console.log('Todo bien'),
+      error: (message) => {
+        Swal.fire('Error', message, 'error');
+      },
+    });
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,10 +1,39 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
+import { environment } from 'src/environments/environments';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly _baseUrl: string = environment.baseUrl;
+  private _http = inject(HttpClient);
 
-  constructor() { }
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
+  public currentUser = computed(() => this._currentUser()); //para no modificar el privado
+  public authStatus = computed(() => this._authStatus()); //para no modificar el privado
+
+  constructor() {}
+
+  login(email: string, password: string): Observable<boolean> {
+    const url = `${this._baseUrl}/auth/login`;
+    const body = { email, password };
+
+    return this._http.post<LoginResponse>(url, body).pipe(
+      tap(({ user, token }) => {
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token', token);
+        console.log({ user, token });
+      }),
+      map(() => true),
+
+      //TODO: errores
+      catchError((err) => throwError(() => err.error.message))
+    );
+  }
 }
