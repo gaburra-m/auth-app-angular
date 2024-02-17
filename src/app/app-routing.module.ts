1cac850c29f7ea3fc38a0702b47164a7a0d7d4be
gaburra-m <gaburra.m@gmail.com>
2024-02-16 19:11:12 -0600

    Sistema de login completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { isAuthenticatedGuard, isNotAuthenticatedGuard } from './auth/guards';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: 'auth',
     //guards
+    canActivate: [isNotAuthenticatedGuard],
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'register',
+    //guards
+    canActivate: [isNotAuthenticatedGuard],
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'dashboard',
     //guards
+    canActivate: [isAuthenticatedGuard],
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
   },
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import {
+  AuthStatus,
+  CheckTokenResponse,
+  LoginResponse,
+  User,
+} from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -17,23 +22,51 @@ export class AuthService {
   public currentUser = computed(() => this._currentUser()); //para no modificar el privado
   public authStatus = computed(() => this._authStatus()); //para no modificar el privado
 
-  constructor() {}
+  constructor() {
+    this.checkAuthStatus().subscribe();
+  }
+
+  private _setAuthentiction(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+
+    return true;
+  }
 
   login(email: string, password: string): Observable<boolean> {
     const url = `${this._baseUrl}/auth/login`;
     const body = { email, password };
 
     return this._http.post<LoginResponse>(url, body).pipe(
-      tap(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-        console.log({ user, token });
-      }),
-      map(() => true),
-
-      //TODO: errores
+      map(({ user, token }) => this._setAuthentiction(user, token)),
       catchError((err) => throwError(() => err.error.message))
     );
+  }
+
+  checkAuthStatus(): Observable<boolean> {
+    const url = `${this._baseUrl}/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      this.logout();
+      return of(false);
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this._http.get<CheckTokenResponse>(url, { headers }).pipe(
+      map(({ token, user }) => this._setAuthentiction(user, token)),
+      catchError(() => {
+        this._authStatus.set(AuthStatus.notAuthenticated);
+        return of(false);
+      })
+    );
+  }
+
+  logout() {
+    this._currentUser.set(null);
+    this._authStatus.set(AuthStatus.notAuthenticated);
+    localStorage.removeItem('token');
   }
 }
